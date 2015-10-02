a86f298fcbcaa4a15bd8246cb51762294afbb6c2
Matias <matiasdecarli@gmail.com>
2015-10-02 15:30:11 -0300

    added ping average response

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 var time_interval = 50;
 var Curl = require('node-curl');
-var url = ''; //test url
+var url = 'url';
+var ok = 0;
+var fail = 0;
+var total = 0;
 
-var requests = 0;
-var response_ok = 0;
+var t_ping = 0;
+
+
+console.log('working...')
 
 setInterval(function() {
-	var curl = Curl.create();
-    curl(url, function(err) {
-        requests++;
+    var curl = Curl.create();
+    var pre = new Date();
 
-        if (this.status && this.status === 200) response_ok++;
+    curl(url, { SSL_VERIFYPEER : 0 }, function(err) {
+        total++;
+        if(this.status && this.status===200) ok++;
+
+               
+        if(this.status!==200)fail++;
+
+        var post = new Date();
+        var time = (post.getTime()) - pre.getTime() ;
+
+        t_ping+= time;
+
         
-        var percentage = response_ok / requests;
-        console.log(this.status, ' : ', (percentage.toFixed(2) * 100) + ' %');
-        this.close();
+        console.log('200: ',ok,'  -   !200:', fail, ' - time: ', time);
     });
 }, time_interval);
 
 
 process.on('SIGINT', function() {
-	console.log('-------------------------------');
-    console.log('FINISHED: ');
-    console.log('requests: ', requests);
-    console.log('requests w/200: ', response_ok);
-
-    var percentage = response_ok / requests;
-    console.log('percentage: ', (percentage.toFixed(2) * 100));
+    console.log('-------------------------------');
+    console.log('FINISHED: ');  
+    console.log('total requests: ', total);
+    console.log('200: ',ok,'   -   !200:', fail, ' - time average: ', t_ping/total);
+    
     process.exit();
 })
