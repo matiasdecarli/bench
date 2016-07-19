7e6d2d96fd1b8a23c84728a14677c34e4d3ee20e
Matias De Carli <matiasdecarli@gmail.com>
2016-07-19 14:31:21 -0300

    update to work with node 4

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-var time_interval = 50;
-var Curl = require('node-curl');
-var url = 'url';
+var Curl = require( 'node-libcurl' ).Curl;
+var time_interval = 100;
+var colors = require('colors');
+
+var url = 'www.google.com';
 var ok = 0;
 var fail = 0;
 var total = 0;
-
 var t_ping = 0;
-
 
 console.log('working...')
 
 setInterval(function() {
-    var curl = Curl.create();
-    var pre = new Date();
+    var curl = new Curl();
 
-    curl(url, { SSL_VERIFYPEER : 0 }, function(err) {
+    curl.setOpt( 'URL', url);
+
+    curl.on( 'error', curl.close.bind( curl ) );
+
+    curl.on( 'end', function( statusCode, body, headers ) {        
         total++;
-        if(this.status && this.status===200) ok++;
-
+        if(statusCode && ((statusCode===200) || (statusCode===301))) ok++;
                
-        if(this.status!==200)fail++;
+        if((statusCode!==200) && (statusCode!=301))fail++;
 
         var post = new Date();
         var time = (post.getTime()) - pre.getTime() ;
 
         t_ping+= time;
-
+        var payload = '200 or 301: ' + ok + '  -   !200:' + fail + ' - time: ' + time; 
         
-        console.log('200: ',ok,'  -   !200:', fail, ' - time: ', time);
-    });
-}, time_interval);
 
+        if (time>2000) return console.log(payload.red);
+        if (time>1000) return console.log(payload.yellow);
+
+        console.log(payload.grey);
+        
+        this.close();
+    });
+
+    var pre = new Date();
+    curl.perform();
+}, time_interval);
 
 process.on('SIGINT', function() {
     console.log('-------------------------------');
